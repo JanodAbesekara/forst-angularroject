ab068074c5be3ba539b403dc387a938d31fbcfea
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-04 09:19:49 +0530

    new edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactUsComponent,
+  },
+
+  {
+    path: 'About',
+    component: AboutUsComponent,
+  },
+];
---
+++ src/app/contact-us/contact-us.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'contact',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './contact-us.component.html',
+  styleUrl: './contact-us.component.css'
+})
+export class ContactUsComponent {
+
+}
