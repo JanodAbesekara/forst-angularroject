e62280ae94e153b1b98beaaab378db0fe617812b
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-04 07:07:03 +0530

    new edit

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
-  imports: [],
+  standalone : true,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
