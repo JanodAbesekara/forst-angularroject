b05f3a24601cee7cf9800e41b37dddfa5afc2194
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-06 06:47:34 +0530

    new edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
   {
@@ -16,5 +17,9 @@ export const routes: Routes = [
   {
     path: 'About',
     component: AboutUsComponent,
+  },
+  {
+    path: 'Projects',
+    component: ProjectsComponent,
   },
 ];
