597f5de79eb25f5d0d4a992171b28db09f1f32d7
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-07-05 11:40:55 +0530

    n

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import emailjs from '@emailjs/browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'contact',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.css'
+  styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
+  from_name: string = '';
+  from_email: string = '';
+  subject: string = '';
+  message: string = '';
 
+  send() {
+    emailjs
+      .send('service_d1u7w98', 'template_hehhwgn', {
+        from_name: this.from_name,
+        to_name: 'Janod',
+        From_email: this.from_email,
+        Subject: this.subject,
+        message: this.message,
+      })
+      .then(
+        (response) => {
+          console.log('SUCCESS!', response.status, response.text);
+        },
+        (err) => {
+          console.error('FAILED...', err);
+        }
+      );
+  }
 }
